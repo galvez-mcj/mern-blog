7dc16bd4527778aded912873966a1f36083a6881
tinay <galvez.mcj@gmail.com>
2023-04-03 21:15:03 +0800

    added login and register page with styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom"
 
 const Login = () => {
   return (
-    <div>
-      Login Page
+    <div className="login-page">
+      <h2>Login</h2>
+      <form>
+      <input 
+          type="text"
+          placeholder="username"
+        />
+        <input
+          type="password"
+          placeholder="password"
+        />
+        <button>Login</button>
+      </form>
+      <div className="login-text">
+        No account yet? <Link to="/register">Register Now!</Link>
+      </div>
     </div>
   )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom"
 
 const Register = () => {
   return (
-    <div>
-      Register Page
+    <div className="register-page">
+      <h2>Register</h2>
+      <form>
+        <input 
+          type="text"
+          placeholder="username"
+        />
+        <input
+          type="password"
+          placeholder="password"
+        />
+        <input
+          type="password"
+          placeholder="confirm password"
+        />
+        <button>Register</button>
+      </form>
+      <div className="register-text">
+        Already have an account? <Link to="/login">Login!</Link>
+      </div>
     </div>
   )
 }
