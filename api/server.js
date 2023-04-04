e6a0b474d385c1eb261b5b47717dad066a7fde51
tinay <galvez.mcj@gmail.com>
2023-04-04 11:10:56 +0800

    connected to mongodb and configured .env

--- api/server.js
+++ api/server.js
@@ -1,17 +1,28 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const app = express()
 
-app.use(cors)
+app.use(cors())
+app.use(express.json())
 
 /* app.get("/api/users", (req, res) => {
     res.json('test ok')
 }) */
 
 app.post('/register', (req, res) => {
-    res.json('register ok')
+    const { username, password } = req.body
+    res.json({ requestData: { username, password } })
 })
 
-app.listen(5000, () => {
-    console.log('listening to port 5000...')
-})
+// connect to db
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`connected to db and listening on port ${process.env.PORT}`)
+        });
+    })
+    .catch( (err) => console.log(err) )
