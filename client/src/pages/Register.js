5e677e7a681150c1b9c1200dd886794f1714861d
tinay <galvez.mcj@gmail.com>
2023-04-04 10:56:26 +0800

    added cors to handle connection error

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -5,15 +5,28 @@ const Register = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [confPassword, setConfPassword] = useState('')
+  const [error, setError] = useState(null)
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const user = { username, password }
+
+    fetch('http://localhost:5000/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user)
+    }).then( () => {
+      console.log('new user registered.')
+    }).catch( err => {
+      setError(err.message)
+    })
   }
 
   return (
     <div className="register-page">
       <h2>Register</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input 
           type="text"
           placeholder="username"
@@ -43,7 +56,11 @@ const Register = () => {
                       ? "btn" : "btn dis"}>
           Register
         </button>
-
+        { error && 
+            <div className="error-container">
+                <b>Error:</b> { error }
+            </div>
+        }
       </form>
       <div className="register-text">
         Already have an account? <Link to="/login">Login!</Link>
